e339a26ccd4bfd7dc0802740d8d4610f306555ed
kassperez <kassandra.perez@utexas.edu>
2016-03-05 07:40:38 -0600

    added more test

--- Test.java
+++ Test.java
@@ -6,8 +6,6 @@
  *
  * EE422C-Assignment 4
  */
-
-
 package assignment4;
 
 import static org.junit.Assert.*;
@@ -15,7 +13,7 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 public class Test {
-
+	
 	@org.junit.Test
 	public void testComputeLadder1() throws NoSuchLadderException {
 		WordLadderSolver wordLadderSolver = new WordLadderSolver();
@@ -38,10 +36,30 @@ public class Test {
 	}
 
 	@org.junit.Test
-	public void testValidateResult() throws NoSuchLadderException {
+	public void testValidateResult1() throws NoSuchLadderException {
 		WordLadderSolver wordLadderSolver = new WordLadderSolver();
 		List<String> result = wordLadderSolver.computeLadder("heads", "tails");
 		assertEquals(true,wordLadderSolver.validateResult("heads","tails",result));
 	}
 
+	@org.junit.Test
+	public void testValidateResult2() throws NoSuchLadderException {
+		WordLadderSolver wordLadderSolver = new WordLadderSolver();
+		List<String> result = wordLadderSolver.computeLadder("heads", "heads");
+		assertEquals(true,wordLadderSolver.validateResult("heads","heads",result));
+	}
+	
+	@org.junit.Test
+	public void testValidateResult3() throws NoSuchLadderException {
+		WordLadderSolver wordLadderSolver = new WordLadderSolver();
+		List<String> result = wordLadderSolver.computeLadder("atlas", "zebra");
+		assertEquals(true,wordLadderSolver.validateResult("atlas", "zebra",result));
+	}
+	
+	@org.junit.Test
+	public void testValidateResult4() throws NoSuchLadderException {
+		WordLadderSolver wordLadderSolver = new WordLadderSolver();
+		List<String> result = wordLadderSolver.computeLadder("atl", "zbsra");
+		assertEquals(false,wordLadderSolver.validateResult("atl", "zbsra",result));
+	}
 }
