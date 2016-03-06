c08e2cb79d900edf8fc74e459091949006b6d660
KassPerez <Kassandra.perez@utexas.edu>
2016-03-06 04:50:46 -0600

    added comments and more testing

--- Test.java
+++ Test.java
@@ -10,6 +10,7 @@ package assignment4;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
@@ -35,6 +36,10 @@ public class Test {
 		assertEquals(null,result);
 	}
 
+	
+	/*	result = a wordLadder
+	 *	Validate result would be true since computeLadder creates a valid result
+	 */
 	@org.junit.Test
 	public void testValidateResult1() throws NoSuchLadderException {
 		WordLadderSolver wordLadderSolver = new WordLadderSolver();
@@ -42,6 +47,10 @@ public class Test {
 		assertEquals(true,wordLadderSolver.validateResult("heads","tails",result));
 	}
 
+	
+	/*	result = 0 since there are no steps in the ladder
+	 *	Validate result would be true 
+	 */
 	@org.junit.Test
 	public void testValidateResult2() throws NoSuchLadderException {
 		WordLadderSolver wordLadderSolver = new WordLadderSolver();
@@ -49,6 +58,10 @@ public class Test {
 		assertEquals(true,wordLadderSolver.validateResult("heads","heads",result));
 	}
 	
+	
+	/*	result = null since there is no wordLadder
+	 *	Validate result would be true 
+	 */
 	@org.junit.Test
 	public void testValidateResult3() throws NoSuchLadderException {
 		WordLadderSolver wordLadderSolver = new WordLadderSolver();
@@ -56,10 +69,69 @@ public class Test {
 		assertEquals(true,wordLadderSolver.validateResult("atlas", "zebra",result));
 	}
 	
+	
+	/*	result = null since there is no wordLadder
+	 *	Validate result would be true 
+	 */
 	@org.junit.Test
 	public void testValidateResult4() throws NoSuchLadderException {
 		WordLadderSolver wordLadderSolver = new WordLadderSolver();
 		List<String> result = wordLadderSolver.computeLadder("atl", "zbsra");
 		assertEquals(true,wordLadderSolver.validateResult("atl", "zbsra",result));
+	}
+	
+	
+	// 2 steps in the ladder change the same position
+	@org.junit.Test
+	public void testValidateResult5() throws NoSuchLadderException {
+		WordLadderSolver wordLadderSolver = new WordLadderSolver();
+		List<String> result = new ArrayList<String>();
+		result.add("heads"); result.add("heals");result.add("teals");result.add("tells");result.add("tills");
+		result.add("gills");result.add("bills");result.add("balls");result.add("bails");result.add("tails");
+		assertEquals(false,wordLadderSolver.validateResult("heads", "tails",result));
+	}
+	
+	
+	// A word in the Ladder is not in the dictionary
+	@org.junit.Test
+	public void testValidateResult6() throws NoSuchLadderException {
+		WordLadderSolver wordLadderSolver = new WordLadderSolver();
+		List<String> result = new ArrayList<String>();
+		result.add("heads"); result.add("heals");result.add("teals");result.add("tells");result.add("talls");
+		result.add("tills");result.add("bills");result.add("balls");result.add("bails");result.add("tails");
+		assertEquals(false,wordLadderSolver.validateResult("heads", "tails",result));
+	}
+	
+	
+	// A word in the ladder is smaller than 5 letters
+	@org.junit.Test
+	public void testValidateResult7() throws NoSuchLadderException {
+		WordLadderSolver wordLadderSolver = new WordLadderSolver();
+		List<String> result = new ArrayList<String>();
+		result.add("heads"); result.add("heals");result.add("teals");result.add("tells");result.add("tills");
+		result.add("bill");result.add("balls");result.add("bails");result.add("tails");
+		assertEquals(false,wordLadderSolver.validateResult("heads", "tails",result));
+	}
+
+	
+	// A word Ladder that doesn't end with the ending word
+	@org.junit.Test
+	public void testValidateResult8() throws NoSuchLadderException {
+		WordLadderSolver wordLadderSolver = new WordLadderSolver();
+		List<String> result = new ArrayList<String>();
+		result.add("heads"); result.add("heals");result.add("teals");result.add("tells");result.add("tills");
+		result.add("bill");result.add("balls");result.add("bails");
+		assertEquals(false,wordLadderSolver.validateResult("heads", "tails",result));
+	}
+	
+	
+	// A step that changes more than one letter
+	@org.junit.Test
+	public void testValidateResult9() throws NoSuchLadderException {
+		WordLadderSolver wordLadderSolver = new WordLadderSolver();
+		List<String> result = new ArrayList<String>();
+		result.add("heads"); result.add("heals");result.add("teals");result.add("tells");result.add("tills");
+		result.add("bill");result.add("balls");result.add("tails");
+		assertEquals(false,wordLadderSolver.validateResult("heads", "tails",result));
 	}
 }
